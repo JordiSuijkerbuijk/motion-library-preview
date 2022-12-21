a14350d73c72c59cd2eb3c2c4788f9bbeafa09b4
Tristan Kirwan <tristankirwan@hotmail.com>
2022-12-21 21:45:26 +0100

    Added fonts

--- src/components/app.js
+++ src/components/app.js
@@ -1,22 +1,22 @@
-import { Router } from 'preact-router';
-
-import Header from './header';
-
+import { Router } from "preact-router";
 // Code-splitting is automated for `routes` directory
-import AnimateGroup from '../routes/animateGroup';
-import Home from '../routes/home';
-import Parallax from '../routes/parallax';
-import style from './style.css';
+import AnimateGroup from "../routes/animateGroup";
+import Home from "../routes/home";
+import Parallax from "../routes/parallax";
+import Sections from "../routes/sections";
+import Header from "./header";
+import style from "./style.css";
 
 const App = () => {
   return (
-    <div id='app'>
+    <div id="app">
       <Header />
       <div class={style.wrapper}>
         <Router>
-          <Home path='/' />
-          <AnimateGroup path='/animateGroup' />
-          <Parallax path='/parallax' />
+          <Home path="/" />
+          <AnimateGroup path="/animateGroup" />
+          <Parallax path="/parallax" />
+          <Sections path="/sections" />
         </Router>
       </div>
     </div>
---
+++ src/routes/sections/index.js
@@ -0,0 +1,9 @@
+const Sections = () => {
+  return (
+    <div>
+      <h1>This is an overview for basic sections</h1>
+    </div>
+  );
+};
+
+export default Sections;
