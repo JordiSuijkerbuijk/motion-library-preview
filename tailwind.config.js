1d3309992a27376e1a36d69762e9976cf6db9877
Tristan <33831870+TristanKirwan@users.noreply.github.com>
2023-01-25 00:07:44 +0100

    Added typography headings base

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  mode: "jit",
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        black: 'hsl(246 6% 9%)',
-        yellow: '#cca43b',
+        black: "hsl(246 6% 9%)",
+        yellow: "#cca43b",
+      },
+      fontFamily: {
+        "proximanova-thin": ["proximanova-thin"],
+        "proximanova-light": ["proximanova-light"],
+        "proximanova-regular": ["proximanova-regular"],
+        "proximanova-bold": ["proximanova-bold"],
+      },
+      fontSize: {
+        "heading-1": ["var(--heading-1)", "var(--heading-1-line-height)"],
+        "heading-2": ["var(--heading-2)", "var(--heading-2-line-height)"],
+        "heading-3": ["var(--heading-3)", "var(--heading-3-line-height)"],
+        "heading-4": ["var(--heading-4)", "var(--heading-4-line-height)"],
+        body: ["var(--body-text)", "var(--body-text-line-height)"],
+        "small-body": [
+          "var(--small-body-text)",
+          "var(--small-body-text-line-height)",
+        ],
       },
       gridTemplateColumns: {
-        6: '6rem',
-        auto: 'auto',
+        6: "6rem",
+        auto: "auto",
       },
       gridTemplateRows: {
-        max: 'max-content',
+        max: "max-content",
       },
     },
   },
